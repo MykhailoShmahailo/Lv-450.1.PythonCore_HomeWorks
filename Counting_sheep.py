faa599fea39ffaf52092df0b2edd267b2a08996c
Shmagailo Mykhailo <zorgmike@gmail.com>
2019-11-24 12:10:42 +0200

    add Count sheep Hometask

---
+++ Counting_sheep.py
@@ -0,0 +1,11 @@
+sheeps = [True,  True,  True,  False,
+           True,  True,  True,  True ,
+           True,  False, True,  False,
+           True,  False, False, True ,
+           True,  True,  True,  True ,
+           False, False, True,  True ]
+
+def count_sheeps(arrayOfSheeps):
+  return arrayOfSheeps.count(True)
+
+print(count_sheeps(sheeps))
