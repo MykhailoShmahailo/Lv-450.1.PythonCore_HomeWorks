f92124ea40ecb33fa6be8a19c626a9ea407d41d0
Shmagailo Mykhailo <zorgmike@gmail.com>
2019-11-24 16:52:59 +0200

    added two more tasks

---
+++ 21_sticks.py
@@ -0,0 +1,4 @@
+def make_move(sticks):
+    return sticks%4
+
+print(make_move(2))
---
+++ No_yelling.py
@@ -0,0 +1,5 @@
+def filter_words(st):
+    st2 = " ".join(st.split()).capitalize()
+    return st2
+
+print(filter_words("HELOO my Libe LALALAL"))
